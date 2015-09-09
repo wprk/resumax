deb7c24b24140644900313cb1638c4f54396a112
Will Parker <Will.Parker@widerplan.com>
2015-09-09 13:56:01 +0100

    Added boilerplate and added in assets folders

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "assets/styles/css/main.css": "assets/styles/less/main.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['assets/styles/less/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
